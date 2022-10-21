bd683059ff1b134a341c2176101f9cf2fe645c04
Vincent Pun <vpun@octoml.ai>
2022-10-21 15:53:44 -0700

    Works locally.

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-OCTOML_AGREE_TO_TERMS=1
-OCTOML_TELEMETRY=false
+export OCTOML_AGREE_TO_TERMS=1
+export OCTOML_TELEMETRY=false
 
 # TODO: Remove the package step and see if we can move it to a separate action.
-./octoml package | ./octoml build | ./octoml deploy
+/octoml package | /octoml build | /octoml deploy
 
 # TODO: Run inference.
