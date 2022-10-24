05a0e49164b28e7f423ebf7161d1303706fcc032
Vincent Pun <vpun@octoml.ai>
2022-10-24 11:44:19 -0700

    Debug with verbose flag on.

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -3,4 +3,4 @@ export OCTOML_AGREE_TO_TERMS=1
 export OCTOML_TELEMETRY=false
 
 # TODO: Remove the package step and see if we can move it to a separate action.
-/octoml package | /octoml build | /octoml deploy
+/octoml package | /octoml build | /octoml deploy -v
