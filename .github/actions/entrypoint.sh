e4a855f61a40bc88e5226c32fc829c5de14228f6
Vincent Pun <vpun@octoml.ai>
2022-10-26 11:24:14 -0700

    Enable backtrace and info.

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 export OCTOML_AGREE_TO_TERMS=1
 export OCTOML_TELEMETRY=false
+export RUST_BACKTRACE=1
+export RUST_LOG=info
 
 # TODO: Remove the package step and see if we can move it to a separate action.
 /octoml package | /octoml build | /octoml -v deploy
